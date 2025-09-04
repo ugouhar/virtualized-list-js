a7c8aed35626bee26d8f88b758f480608a0396fa
ugouhar <ushran987@gmail.com>
2025-09-04 16:07:48 +0530

    Refactor

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const NUMBER_OF_CARDS = 300;
+export const ITEM_HEIGHT = 50;
--- script.js
+++ script.js
@@ -1,86 +1,84 @@
-const container = document.getElementById("container");
-const numberOfCards = 300;
-const ITEM_HEIGHT = 50;
-const items = Array(numberOfCards)
-  .fill(0)
-  .map((item, index) => ({ value: `Card ${index + 1}`, top: 0 }));
+import { ITEM_HEIGHT, NUMBER_OF_CARDS } from "./constants.js";
 
+const container = document.getElementById("container");
 let previousScrollTop = 0;
-let scrollTop = 0;
-const createNewCard = (item) => {
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.textContent = item.value;
-  card.style.top = `${item.top}px`;
-  return card;
-};
+let currentScrollTop = 0;
+const dataList = generateDataList();
+
+function generateDataList() {
+  const data = Array(NUMBER_OF_CARDS)
+    .fill(0)
+    .map((_, index) => ({ value: `Card ${index + 1}`, top: 0 }));
+  return data;
+}
 
 const addSpacerElement = () => {
   const spacer = document.createElement("div");
   spacer.classList.add("spacer");
-  spacer.style.height = `${numberOfCards * ITEM_HEIGHT}px`;
+  spacer.style.height = `${NUMBER_OF_CARDS * ITEM_HEIGHT}px`;
   container.appendChild(spacer);
 };
 
-const addCards = (cardList = []) => {
-  cardList.forEach((item) => {
-    container.appendChild(createNewCard(item));
+const addItem = (data) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.textContent = data.value;
+  card.style.top = `${data.top}px`;
+  return card;
+};
+
+const addItemsList = (itemList = []) => {
+  itemList.forEach((item) => {
+    container.appendChild(addItem(item));
   });
 };
 
+const addInitialItems = () => {
+  const itemHeight = getItemHeight();
+  const slicedItem = dataList.slice(0, 10).map((item, index) => ({
+    ...item,
+    top: currentScrollTop + index * itemHeight,
+  }));
+  addSpacerElement();
+  addItemsList(slicedItem);
+};
+
 const getItemHeight = () => {
-  // const borderWidth = 1;
-  // const itemHeight =
-  //   document.querySelector(".card")?.clientHeight ||
-  //   ITEM_HEIGHT + 2 * borderWidth;
-  // return itemHeight;
   return ITEM_HEIGHT;
 };
 
 const getContainerHeight = () => {
-  const containerHeight = document.querySelector("#container").clientHeight;
-
-  return containerHeight;
-};
-const getNumberOfItemsInViewPort = () => {
-  const numberOfItemsInViewPort = getContainerHeight() / getItemHeight();
-  return numberOfItemsInViewPort;
+  return document.getElementById("container").clientHeight;
 };
 
-container.addEventListener("scroll", (e) => {
-  scrollTop = e.target.scrollTop;
+const getNumberOfItemsWithinViewport = () => {
+  return getContainerHeight() / getItemHeight();
+};
+
+const handleContainerScroll = (e) => {
+  currentScrollTop = e.target.scrollTop;
   const itemHeight = getItemHeight();
-  let itemsAboveViewport = Math.floor(scrollTop / itemHeight);
+  const itemsAboveViewport = Math.floor(currentScrollTop / itemHeight);
+  const itemsWithinViewport = getNumberOfItemsWithinViewport();
 
-  const numberOfItemsInViewPort = getNumberOfItemsInViewPort();
-
-  if (Math.abs(scrollTop - previousScrollTop) >= itemHeight) {
-    const slicedItem = items
+  if (Math.abs(currentScrollTop - previousScrollTop) >= itemHeight) {
+    const slicedItem = dataList
       .slice(
-        itemsAboveViewport,
-        itemsAboveViewport + numberOfItemsInViewPort + 5
+        Math.max(0, itemsAboveViewport),
+        itemsAboveViewport + itemsWithinViewport + 5
       )
       .map((item, index) => ({
         ...item,
         top:
-          Math.floor(scrollTop / ITEM_HEIGHT) * ITEM_HEIGHT +
+          Math.floor(currentScrollTop / ITEM_HEIGHT) * ITEM_HEIGHT +
           index * itemHeight,
       }));
-    console.log(scrollTop, previousScrollTop, scrollTop / itemHeight);
-    previousScrollTop = scrollTop;
+    previousScrollTop = currentScrollTop;
     container.innerHTML = "";
     addSpacerElement();
-    addCards(slicedItem);
+    addItemsList(slicedItem);
   }
-});
-
-const addInitialItems = () => {
-  const itemHeight = getItemHeight();
-  const slicedItem = items.slice(0, 10).map((item, index) => ({
-    ...item,
-    top: scrollTop + index * itemHeight,
-  }));
-  addSpacerElement();
-  addCards(slicedItem);
 };
+
+container.addEventListener("scroll", handleContainerScroll);
 addInitialItems();
